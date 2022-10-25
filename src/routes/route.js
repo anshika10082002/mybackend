70c0e544518f36fdf1acc1db94edf9721304d6e9
anshika10082002 <anshika10082002@gmail.com>
2022-10-25 14:59:27 +0530

    pushed mongo assign 1 branch

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const BookModel= require("../models/bookModel.js")
+const BookController= require("../controllers/bookController");
+//const bookModel = require('../models/bookModel.js');
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
 
-module.exports = router;
+
+router.post("/createBook" , BookController.createBook)
+
+router.get("/getBookData", BookController.getBookData)
+
+
+ module.exports = router;
